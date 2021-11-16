23f5310ef0bb84611deae66b16b72a9308d94914
Michael Perk <m.perk@tu-bs.de>
2021-11-16 13:42:20 +0100

    Some basic design cards for twitter and articles

---
+++ src/components/profiles/ArticleCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card
+      class="slider-card article-card d-flex flex-column justify-space-between"
+      dark
+      rounded
+      :style="image!==null? { 'background-image': 'url(' + image + ')' }:{}"
+  >
+    <v-card-title>
+      <span class="font-weight-light text-caption ml-auto">{{ article.published }}</span>
+    </v-card-title>
+
+
+    <v-card-text class="font-weight-bold">
+      {{ article.title }}
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "ArticleCard",
+  props: {
+    article: Object
+  },
+  data() {
+    return {
+      test: "test",
+      image: this.article.images.length > 0? this.article.images[0].url : null
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .article-card {
+    background-position: center; /* Center the image */
+    background-repeat: no-repeat; /* Do not repeat the image */
+    background-size: cover; /* Resize the background image to cover the entire container */
+    background-color: #5e5e5e;
+    background-blend-mode: multiply;
+  }
+</style>
--- src/views/Politician.vue
+++ src/views/Politician.vue
@@ -1,15 +1,77 @@
 <template>
   <div>
-    <h1>Politician {{ id }}</h1>
+    <v-container>
+      <v-row>
+        <v-col class="text-center" lg="6">
+          <v-avatar class="mx-5 mt-3" size="128">
+            <img :alt="politician.label" :src="politician.image" class="avatar-image">
+          </v-avatar>
+          <h2 class="mt-1 grey--text text--darken-2">{{ politician.label }}</h2>
+        </v-col>
+
+        <v-col lg="6">
+          <v-list two-line>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>Partei</v-list-item-title>
+                <v-list-item-subtitle>{{ politician.party.label }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>Beruf</v-list-item-title>
+                <v-list-item-subtitle>{{ politician.occupation }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>Ausbildung</v-list-item-title>
+                <v-list-item-subtitle>{{ politician.education }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <div class="px-5">
+      <h3 class="mb-2">Aktuelle Berichte</h3>
+      <v-slide-group
+          multiple
+          show-arrows
+          class="overflow-y-visible"
+      >
+        <v-slide-item
+            v-for="tweet in politician.tweets"
+            :key="tweet.id"
+        >
+          <TweetCard :tweet="tweet" :user="politician.twitterUser"/>
+        </v-slide-item>
+        <v-slide-item
+            v-for="article in politician.articles"
+            :key="article.id"
+        >
+          <ArticleCard :article="article"/>
+        </v-slide-item>
+      </v-slide-group>
+    </div>
   </div>
 </template>
 
 <script>
+import mockup from "../assets/politicians/mockup.json"
+import TweetCard from "../components/profiles/TweetCard";
+import ArticleCard from "../components/profiles/ArticleCard";
 export default {
   name: "Politician",
+  components: {
+    TweetCard,
+    ArticleCard
+  },
   data: () => {
     return {
-      id: null
+      id: null,
+      politician: mockup
     }
   },
   mounted() {
@@ -18,6 +80,18 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+.avatar-image {
+  object-fit: cover
+}
 
+.slider-card {
+  width: 300px;
+  margin-left: 1em;
+  margin-right: 1em;
+}
+
+.overflow-y-visible div {
+  overflow-y: visible !important;
+}
 </style>
