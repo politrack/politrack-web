0b430401df4099d8b3b8281bbce670d7940bf400
Jonas Dippel <jonas.dippel@gmx.de>
2021-11-17 12:22:18 +0100

    improved general layout

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
 <template>
   <v-app>
-    <v-app-bar app color="white" light>
-      <v-spacer></v-spacer>
-
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="./assets/logo.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <h1 class="grey--text">PoliTrack</h1>
-      </div>
-
-      <v-spacer></v-spacer>
-
+    <v-app-bar app flat color="transparent">
+      <v-container>
+        <v-row>
+          <div class="d-flex align-center">
+            <v-img
+                alt="PoliTrack Logo"
+                class="shrink mr-2"
+                contain
+                src="./assets/logo.png"
+                transition="scale-transition"
+                width="30"
+            />
+            <h3 class="grey--text text--darken-3">PoliTrack</h3>
+          </div>
+          <v-spacer></v-spacer>
+          <a
+              class="icon-link"
+              href="https://github.com/themeselection/materio-vuetify-vuejs-admin-template-free"
+              target="_blank"
+              rel="nofollow"
+          >
+            <v-icon class="">
+              fab fa-github
+            </v-icon>
+            <v-icon class="ms-3">
+            fab fa-twitter
+            </v-icon>
+          </a>
+        </v-row>
+      </v-container>
     </v-app-bar>
 
     <v-main :style="{background: $vuetify.theme.themes[theme].background}" class="mt-5">
@@ -27,16 +39,16 @@
 </template>
 
 <script>
+
+
 export default {
   name: 'App',
 
-  components: {
-  },
+  components: {},
 
-  data: () => ({
-  }),
-  computed:{
-    theme(){
+  data: () => ({}),
+  computed: {
+    theme() {
       return (this.$vuetify.theme.dark) ? 'dark' : 'light'
     }
   }
@@ -44,5 +56,13 @@ export default {
 </script>
 
 <style>
+
+.v-toolbar__content {
+  background: rgb(245, 245, 245)
+}
+
+.icon-link {
+  text-decoration: none;
+}
 
 </style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,18 +1,20 @@
 <template>
-  <div>
-    <h1>Derzeit beliebt</h1>
-    <v-row class="px-5">
-      <v-col
-          v-for="politician in politicians"
-          :key="politician.id"
-          cols="12"
-          lg="4"
-          sm="12"
-      >
-        <PoliticiansCard :politician="politician"/>
-      </v-col>
-    </v-row>
-  </div>
+  <v-container>
+    <div>
+      <h2 class="grey--text text--darken-2">Politiker im Fokus</h2>
+      <v-row class="px-5 mt-2">
+        <v-col
+            v-for="politician in politicians"
+            :key="politician.id"
+            cols="12"
+            lg="4"
+            sm="12"
+        >
+          <PoliticiansCard :politician="politician"/>
+        </v-col>
+      </v-row>
+    </div>
+  </v-container>
 </template>
 
 <script>
