7e4569718ec48fcb52d7ee1ec509d07e595723c7
Jonas Dippel <jonas.dippel@gmx.de>
2022-02-23 14:50:35 +0100

    added not found page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+
+    <h1 class="text-center mt-5">404 - Seite nicht gefunden</h1>
+    <div class="text-muted mt-2 text-center">
+      Die angeforderte Seite wurde leider nicht gefunden.<br>
+      <router-link class="text-decoration-none" :to="{name: 'chronic'}">Zurück zur Hauptseite</router-link>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound"
+}
+</script>
+
+<style scoped>
+
+</style>
