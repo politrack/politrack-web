7dff65f6bc0e4a588ac89a108cec77eee1dbf4a9
Jonas Dippel <jonas.dippel@gmx.de>
2022-02-25 20:42:34 +0100

    added abgeordentenwatch url and removed twitter

--- src/components/profiles/ProfileCard.vue
+++ src/components/profiles/ProfileCard.vue
@@ -11,18 +11,18 @@
 
       <v-col md="7" cols="12">
         <v-chip
-            v-for="chip in chips"
-            :key="chip.text"
+            v-for="chip in chips" :key="chip.text"
             class="ma-1"
             :color="chip.color ? chip.color : '#f0f0f0'"
             :text-color="chip.color ? '#ffffff' : '#333333'"
             label>
-          <v-icon left small>
-            {{ chip.icon }}
-          </v-icon>
-          {{ chip.text }}
+          <a :href="chip.url" style="text-decoration: none; color: inherit">
+            <v-icon left small>
+              {{ chip.icon }}
+            </v-icon>
+            {{ chip.text }}
+          </a>
         </v-chip>
-
         <div class="d-flex align-center">
           <span class="text-caption me-4">Oft erwähnt mit:</span>
           <PoliticianAvatarRow :persons="mentionedWith.slice(0,5)" class="mt-5"/>
@@ -50,39 +50,40 @@ export default {
     statistics: Object
   },
   data: function () {
-
-    let occupation = this.politician.occupation.split(",").map(function(item){
+    let chips = this.politician.occupation.split(",").map(function (item) {
       return {
         'icon': 'fas fa-tasks',
         'text': item.trim()
       }
     })
-
+    if (this.politician.education) {
+      chips.push({
+        'icon': 'fas fa-school',
+        'text': this.politician.education
+      })
+    }
+    if (this.politician.year_of_birth) {
+      chips.push({
+        'icon': 'fas fa-birthday-cake',
+        'text': this.politician.year_of_birth
+      })
+    }
+    if (this.statistics.basic.lastWeekArticles) {
+      chips.push({
+        'icon': 'fas fa-newspaper',
+        'text': this.statistics.basic.lastWeekArticles + ' Artikel letzte Woche'
+      })
+    }
+    if (this.politician.abgeordnetenwatch_url) {
+      chips.push({
+        'icon': 'fas fa-external-link-alt',
+        'text': 'Abgeordnetenwatch',
+        'color': '#f46b3b',
+        'url': this.politician.abgeordnetenwatch_url
+      })
+    }
     return {
-      chips: occupation.concat([
-        {
-          'icon': 'fas fa-school',
-          'text': this.politician.education
-        },
-        {
-          'icon': 'fas fa-birthday-cake',
-          'text': this.politician.year_of_birth
-        },
-        {
-          'icon': 'fas fa-newspaper',
-          'text': this.statistics.basic.lastWeekArticles + ' Artikel letzte Woche'
-        },
-        {
-          'icon': 'fas fa-external-link-alt',
-          'text': 'Abgeordnetenwatch',
-          'color': '#f46b3b'
-        },
-        {
-          'icon': 'fab fa-twitter',
-          'text': 'Twitter',
-          'color': '#1DA1F2'
-        }
-      ])
+      chips: chips
     }
   }
 }
